b9d764e2d6d4bebeb021a201bcc515b04ff90227
tiagolimar <tiagolima.r2@gmail.com>
2023-04-03 23:43:37 -0300

    melhorias de navegabilidade

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 let scores = [];
 let total_score = 0;
-let questions = document.querySelectorAll('.question');
+let id_question_active = 1;
+
 let question_active = document.querySelector('.active');
 let item_active = document.querySelector('.active-item');
-let result = document.querySelector('.result');
 
-function active_question(){
-    id_question = event.target.getAttribute('value');
-    
+const questions = document.querySelectorAll('.question');
+const result = document.querySelector('.result');
+const button = document.querySelector('#btn-mor');
+const next_button = document.querySelector('.next');
+const back_button = document.querySelector('.back');
+
+
+function active_question(id_question=-1){
+    if(id_question == -1){
+        id_question = event.target.getAttribute('value');
+    }
+
     item_active.classList.remove('active-item');
-    item_active = event.target;
+    item_active = document.querySelector(`div[value=${id_question}]`);
     item_active.classList.add('active-item');
 
     question_active.classList.remove('active');
     question_active = document.querySelector(`#${id_question}`);
     question_active.classList.add('active');
+}
+
+function active_before_question(){
+    id_question_active -= 1;
+
+    if (id_question_active==1){
+        back_button.classList.add('hidden');
+    }else if(id_question_active==questions.length-1){
+        next_button.classList.remove('hidden');
+    }
+    active_question(`q${id_question_active}`);
+}
+
+function active_next_question(){
+    id_question_active += 1;
+
+    if (id_question_active==questions.length){
+        next_button.classList.add('hidden');
+    }else if(id_question_active == 2){
+        back_button.classList.remove('hidden');
+    }else{
+        next_button.classList.remove('hidden');
+    }
+
+    active_question(`q${id_question_active}`);
 }
 
 function update_total_score(){
@@ -24,6 +58,7 @@ function update_total_score(){
     }
 
     if(!scores.includes(-1)){
+        button.classList.remove('hidden');
         if (0 < total_score && total_score < 5){
             result.innerHTML = 'Impossível de interpretar';
         }else if (4 < total_score && total_score < 11){
@@ -62,7 +97,6 @@ function show_result(){
 }
 
 function create_scores(){
-    questions = document.querySelectorAll('.question');
     for (question of questions) {
         scores.push(-1);
     }
